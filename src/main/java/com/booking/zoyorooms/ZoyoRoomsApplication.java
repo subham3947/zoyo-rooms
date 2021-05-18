66082d43956c6b55ddb50c62efcbad6912381e90
SUBHAM YADAV <subhamyadav2700@gmail.com>
2021-05-18 08:32:03 +0530

    Initial setup done

---
+++ src/main/java/com/booking/zoyorooms/ZoyoRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.booking.zoyorooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoyoRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoyoRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/zoyorooms/ZoyoRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.zoyorooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoyoRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
